b34fab31ab5a059bb62cfac06ae961f295599eaa
Takahiro <tkhr_621@docomo.ne.jp>
2020-03-23 22:33:18 +0900

    ESLintの設定ファイルの追加

---
+++ .eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+    env: {
+        browser: true, // document や console にエラーが出ないようにする
+        es6: true, // es6から使える let や const にエラーがでないようにする
+    },
+    extends: ['eslint:recommended', 'plugin:prettier/recommended'], // ESLintの推奨設定をベースにする
+};
